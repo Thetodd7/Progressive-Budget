8c0cd6a47c5383c26bb4f7813986f9bedb2b0cdc
apple <apple@apples-MacBook-Pro.local>
2021-08-31 13:17:26 -0400

    server

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-// const viewCont = require("./controllers/view");
-// const apiCont = require("./controllers/api");
-const controllers = require("./controllers")
+const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 app.use(express.static("public"));
 
@@ -23,10 +21,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/money', {
     useCreateIndex: true,
     useFindAndModify: false,
 });
-
-// app.use(apiCont);
-app.use(controllers);
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
